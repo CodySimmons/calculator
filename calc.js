6c93ff5e71c4dce489908efa29ad1fcec5558218
Cody Simmons <devCodySimmons@gmail.com>
2021-02-02 23:19:45 -0700

    Calculator Overall Style Completed. Calculator html/css has overall div areas set up with styling. Calculator js event listeners for buttons have been created.
    index.css
    -Added grid overlay to screen display.
    -Added shadows to buttons and inset screen
    -Added button texture png
    -Background pinstripe
    -Adjusted github footer logo for dark theme
    
    calc.js
    -global variables for buttons and display set
    -inputRecorder created for button event listener and future math solving
    -display area textContent setup for simple number & operator displaying

--- calc.js
+++ calc.js
@@ -1,3 +1,72 @@
+const calcButtons = document.querySelectorAll(".calculator-button");
+const display = document.querySelector("#input-numbers");
+var displayNumber = "";
+var displayHistory = "";
+
+function inputRecorder(input) {
+  button = this.id;
+  switch (button) {
+    case "one-button":
+      displayNumber = displayNumber + "1";
+      break;
+    case "two-button":
+      displayNumber = displayNumber + "2";
+      break;
+    case "three-button":
+      displayNumber = displayNumber + "3";
+      break;
+    case "four-button":
+      displayNumber = displayNumber + "4";
+      break;
+    case "five-button":
+      displayNumber = displayNumber + "5";
+      break;
+    case "six-button":
+      displayNumber = displayNumber + "6";
+      break;
+    case "seven-button":
+      displayNumber = displayNumber + "7";
+      break;
+    case "eight-button":
+      displayNumber = displayNumber + "8";
+      break;
+    case "nine-button":
+      displayNumber = displayNumber + "9";
+      break;
+    case "divide-button":
+      displayNumber = displayNumber + "/";
+      break;
+    case "multiply-button":
+      displayNumber = displayNumber + "X";
+      break;
+    case "minus-button":
+      displayNumber = displayNumber + "-";
+      break;
+    case "decimal-button":
+      displayNumber = displayNumber + ".";
+      break;
+    // case "negative-button":
+    //   displayNumber = displayNumber + '/';
+    //   break;
+    case "add-button":
+      displayNumber = displayNumber + "+";
+      break;
+    case "clear-button":
+      displayNumber = "";
+      break;
+    case "all-clear-button":
+      displayNumber = "";
+      break;
+    // case "equal-button":
+    //   displayNumber = "";
+    //   break;
+    default:
+      break;
+  }
+  console.log(displayNumber);
+  display.textContent = displayNumber;
+}
+
 function add(array) {
   return array[0] + array[1];
 }
@@ -36,11 +105,4 @@ function operate(mathArray) {
   }
 }
 
-operate([1, 2, "plus"]);
-
-operate([1, 2, "minus"]);
-
-operate([5, 5, "multiply"]);
-
-operate([5.0, 3, "divide"]);
-//operate([25, 0, "divide"]);
+calcButtons.forEach((btn) => btn.addEventListener("click", inputRecorder));
